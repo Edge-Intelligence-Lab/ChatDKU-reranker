4d4b8058bd505b6e93683533b1572f7b3cd5737f
pomegranar <an301@duke.edu>
2026-01-29 02:51:50 +0000

    added reranker spawn script

---
+++ run-qwen3-vl-reranker-sglang.sh
@@ -0,0 +1,9 @@
+HF_HOME=/datapool/huggingface/ HF_ENDPOINT=https://hf-mirror.com ./.venv/bin/python3 -m sglang.launch_server \
+  --host 0.0.0.0 \
+  --port 30000 \
+  --model-path Qwen/Qwen3-VL-Reranker-8B \
+  --trust-remote-code \
+  --dtype auto \
+  --tp-size 1 \
+  # --chat-template none
+
