fff13e559b33a3355dcf587cbf1186649574f1e2
pomegranar <an301@duke.edu>
2026-02-04 12:50:55 +0000

    Added test runs, still need testing tho

--- run-qwen3-vl-reranker-vllm.sh
+++ run-qwen3-vl-reranker-vllm.sh
@@ -1,6 +1,8 @@
 VISIBLE_CUDA_DEVICES=0 HF_HOME=/datapool/huggingface/ HF_ENDPOINT=https://hf-mirror.com ./.venv/bin/python3 \
   -m vllm.entrypoints.openai.api_server \
   --model Qwen/Qwen3-VL-Reranker-8B \
+  --trust-remote-code \
+  --task score \
   --dtype bfloat16 \
   --host 0.0.0.0 \
   --port 6767
