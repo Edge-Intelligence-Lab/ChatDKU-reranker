d1729658451f83615eaaa727b79cbbb0fdb246b1
pomegranar <an301@duke.edu>
2026-02-05 02:35:30 +0000

    working version for reranker run script using VLLM

--- run-qwen3-vl-reranker-vllm.sh
+++ run-qwen3-vl-reranker-vllm.sh
@@ -2,7 +2,6 @@ VISIBLE_CUDA_DEVICES=0 HF_HOME=/datapool/huggingface/ HF_ENDPOINT=https://hf-mir
   -m vllm.entrypoints.openai.api_server \
   --model Qwen/Qwen3-VL-Reranker-8B \
   --trust-remote-code \
-  --task score \
   --dtype bfloat16 \
   --host 0.0.0.0 \
   --port 6767
